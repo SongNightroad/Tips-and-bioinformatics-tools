8f9e3db6354cb2227211617cc0fdd8054408ca20
SongNightroad <90181447+SongNightroad@users.noreply.github.com>
2022-04-29 15:31:21 +0800

    both bedtools and samtools scripts for xgen panel
    just an example

--- bedtools on five genes.bash
+++ bedtools on five genes.bash
@@ -1,10 +1,12 @@
 #!/bin/sh
-ls ./data/*.bqsr.bam > names_of_samples
-file="names_of_samples"
+ls  //core-pool/storage/users/apetukhova/bones_exomes/calling_results/xgen_controls/*.bam > names_of_samples_xgen_controls
+file="names_of_samples_xgen_controls"
 while read line; do
-line_2=${line:7}
-bedtools coverage -a ./data/apoc2.bed -b $line -d > apoc2.$line_2.bedcoverage
-bedtools coverage -a ./data/apoa5.bed -b $line -d > apoa5.$line_2.bedcoverage
-bedtools coverage -a ./data/lmf1.bed -b $line -d > lmf1.$line_2.bedcoverage
-bedtools coverage -a ./data/gpihbp1.bed -b $line -d > gpihbp1.$line_2.bedcoverage
+line_2=${line:80}
+echo "start with $line file"
+bedtools coverage -a ./data/CPNE9 -b $line -d > ./bedcoverage_xgen_controls/CPNE9.$line_2.bedcoverage
+bedtools coverage -a ./data/SHPRH -b $line -d > ./bedcoverage_xgen_controls/SHPRH.$line_2.bedcoverage
+bedtools coverage -a ./data/RAB31 -b $line -d > ./bedcoverage_xgen_controls/RAB31.$line_2.bedcoverage
+samtools view -c -F 260 $line > ./samtools_xgen_controls/$line_2.samtools
+echo "done with $line file"
 done < $file
